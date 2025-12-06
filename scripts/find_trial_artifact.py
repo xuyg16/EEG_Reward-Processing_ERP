a61deec630672420c8c378d652b6dedd961b1ba9
Zheng <zhenglin1017@gmail.com>
2025-12-06 23:46:30 +0100

    Added ERP analysis and artifact script changes

--- scripts/find_trial_artifact.py
+++ scripts/find_trial_artifact.py
@@ -23,21 +23,22 @@ def find_artifacts(trials, maxMin=500e-6, level=500e-6, step=40e-6, lowest=0.1e-
     # CHECKPOINT 1: MaxMin
     max_amp = np.max(data, axis=2)
     min_amp = np.min(data, axis=2)
-    is_minMax_artifact = (max_amp - min_amp) > maxMin
+    is_minMax_artifact = np.abs(max_amp - min_amp) > maxMin
     is_artifact |= is_minMax_artifact      # return True if either of left or right variable is True
 
     # CHECKPOINT 2: Level
-    is_level_artifact = max_amp > level
+    is_level_artifact =  np.max(np.abs(data), axis=2)  > level
     is_artifact |= is_level_artifact
 
     # CHEKPOINT 3: Step
     diff = np.diff(data, axis=2)
-    is_step_artifact = np.any(np.abs(diff) > step, axis=2)
+    is_step_artifact = np.any(diff > step, axis=2)
     is_artifact |= is_step_artifact
 
     # CHECKPOINT 4: lowest
     is_lowest_artifact = np.all(np.abs(data) < lowest, axis=2)
     is_artifact |= is_lowest_artifact
+
 
 
     return is_artifact
