59c610560cd0c52cfd9f942a92221a0cd4c45d07
QIANYUE-LIII <ivyapple14@outlook.com>
2026-01-04 15:59:56 +0100

    -  updated comments for step1

--- scripts/s01_downsample_filter.py
+++ scripts/s01_downsample_filter.py
@@ -1,32 +1,65 @@
 from meegkit.dss import dss_line
 import numpy as np
-import mne
 
-def down_sampling(raw, new_sfreq=250):
-    eeg_down = raw.copy().resample(new_sfreq, npad='auto')
-    print(f"Original Sampling Rate: {raw.info['sfreq']} Hz")
+def down_sampling(eeg, new_sfreq=250):
+    '''
+    Downsample the eeg signal.
+    
+    :param eeg: eeg signal to be processed
+    :param new_sfreq: the frequency after downsampling
+
+    output: downsampled eeg signal
+    '''
+    eeg_down = eeg.copy().resample(new_sfreq, npad='auto')
+    print(f"Original Sampling Rate: {eeg.info['sfreq']} Hz")
     print(f"New Sampling Rate: {eeg_down.info['sfreq']} Hz")
     
     return eeg_down
 
 
-def band_filter(eeg_down, f_low=0.1, f_high=30):
-    # manipulate the cutoff frequencies if needed
-    eeg_band = eeg_down.copy().filter(l_freq=f_low, h_freq=f_high)
+# NOTE: add and manipulate the cutoff frequencies if needed
+def band_filter(eeg, f_low=0.1, f_high=30):
+    '''
+    Perform bandpass filtering on the downsampled eeg signal.
+    
+    :param eeg: eeg signal to be processed
+    :param f_low: low cutoff frequency
+    :param f_high: high cutoff frequency
+
+    output: bandpass filtered eeg signal
+    '''
+    eeg_band = eeg.copy().filter(l_freq=f_low, h_freq=f_high)
 
     return eeg_band
 
 
-def notch_filter(eeg_band, line_freq=50):
-    eeg_band_notch = eeg_band.copy().notch_filter(line_freq)
+def notch_filter(eeg, line_freq=50):
+    '''
+    Perform notch filtering on the bandpass filtered eeg signal.
+
+    :param eeg: eeg signal to be processed
+    :param line_freq: line frequency to be removed
+
+    output: notch filtered eeg signal
+    '''
+    eeg_band_notch = eeg.copy().notch_filter(line_freq)
 
     return eeg_band_notch
 
-def zapline_filter(eeg_band, line_freq=50):
+
+def zapline_filter(eeg, line_freq=50):
+    '''
+    Perform zapline filtering on the bandpass filtered eeg signal.
+    
+    :param eeg: eeg signal to be processed
+    :param line_freq: line frequency to be removed
+
+    output: zapline filtered eeg signal
+    '''
     # input & output of dss_line are of shape: (n_samples, n_channels, n_trial)
-    eeg_band_zap = eeg_band.copy()
-    band_sfreq = eeg_band.info['sfreq']
-    eeg_band_zap_array, _ = dss_line(np.expand_dims(eeg_band.get_data().T, axis=2), fline=50, sfreq=band_sfreq)
+    eeg_band_zap = eeg.copy()
+    band_sfreq = eeg.info['sfreq']
+    eeg_band_zap_array, _ = dss_line(np.expand_dims(eeg.get_data().T, axis=2), fline=line_freq, sfreq=band_sfreq)
     # convert back to shape: (n_channels, n_samples)
     eeg_band_zap_array = eeg_band_zap_array.squeeze().T
     eeg_band_zap._data = eeg_band_zap_array
