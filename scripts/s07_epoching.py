19f72fedf116d4a0a8f3c194f9c943d6f8981700
QIANYUE-LIII <ivyapple14@outlook.com>
2026-01-23 12:37:21 +0100

    fixed and moved getting evts_dict to tools.py

--- scripts/s07_epoching.py
+++ scripts/s07_epoching.py
@@ -1,5 +1,6 @@
 import mne
 from s03_07_trial_rejection import trial_rejection_cust, trial_rejection_mne
+from tools import get_event_dict
 
 def epoching(conditions_dict, eeg, max=150e-6, min=0.1e-6, tmin=-0.2, tmax=0.6, baseline=(-0.2, 0)):
     '''
@@ -16,14 +17,14 @@ def epoching(conditions_dict, eeg, max=150e-6, min=0.1e-6, tmin=-0.2, tmax=0.6, 
 
     :return: MNE Epochs object containing the epoched and cleaned data
     '''
-    evts, event_id = mne.events_from_annotations(eeg)
 
+    # get all the feedback-locked stimulus
     all_markers = []
     for markers in conditions_dict.values():
         all_markers.extend(markers)
 
     # Create the filtered event dictionary
-    evts_dict_stim = {k: event_id[k] for k in event_id.keys() if k in all_markers}
+    evts, evts_dict_stim = get_event_dict(eeg, all_markers)
 
     epochs_all = trial_rejection_mne(eeg, evts, evts_dict_stim, max=max, min=min,  tmin=tmin, tmax=tmax, baseline=baseline)
 
@@ -45,14 +46,13 @@ def epoching_cust(conditions_dict, eeg, maxMin=150e-6, level=150e-6, step=40e-6,
 
     :return: MNE Epochs object containing the epoched and cleaned data
     '''
-    evts, event_id = mne.events_from_annotations(eeg)
-
+    
     all_markers = []
     for markers in conditions_dict.values():
         all_markers.extend(markers)
 
     # Create the filtered event dictionary
-    evts_dict_stim = {k: event_id[k] for k in event_id.keys() if k in all_markers}
+    evts, evts_dict_stim = get_event_dict(eeg, all_markers)
 
     epochs_all = trial_rejection_cust(eeg, evts, evts_dict_stim, maxMin=maxMin, level=level, step=step, lowest=lowest
                                                        , tmin=tmin, tmax=tmax, baseline=baseline)
--- scripts/tools.py
+++ scripts/tools.py
@@ -1,3 +1,5 @@
+import mne
+
 def get_valid_input(prompt, valid_options):
     """
     Prompt the user for input until a valid option is provided.
@@ -13,3 +15,20 @@ def get_valid_input(prompt, valid_options):
             return user_input
         else:
             print(f"Invalid input. Please choose from {valid_options}.")
+
+
+
+def get_event_dict(eeg, condition_dict):
+    '''
+    get the dictionary for the events under specified conditions
+    
+    :param eeg: input eeg data in mne.Raw format
+    :param condition_dict: conditions for epoching
+
+    :return evts: identity and timing of the epochs. 
+    :return conidtion_dict: events dictionary filtered by conditions
+    '''
+    evts, evts_dict = mne.events_from_annotations(eeg)
+    evts_dict_stim = {k: evts_dict[k] for k in evts_dict.keys() if k in condition_dict}
+    
+    return evts, evts_dict_stim
